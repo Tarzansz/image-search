6bdaf43765c6643a590066078d808dc8d20486c8
Tarzansz <tarzanpansz@gmail.com>
2025-03-18 15:55:15 +0800

    Add src/styles/GlobalStyle.js

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    background-color: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.text};
+    line-height: 1.6;
+  }
+
+  a {
+    color: ${props => props.theme.colors.primary};
+    text-decoration: none;
+    
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  button, input, textarea {
+    font-family: inherit;
+  }
+`;
+
+export default GlobalStyle;
